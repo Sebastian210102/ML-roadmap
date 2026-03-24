135d9839160bf2dda66bcef4782ba8a2331fdf9b
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-03-24 11:02:32 -0600

    Add exercise about Numpy fundamentals

--- Fundamentals/Numpy/01_numpy_basics.py
+++ Fundamentals/Numpy/01_numpy_basics.py
@@ -31,3 +31,32 @@ determinant = np.linalg.det(matrix)
 print("Determinante de la matriz formada por los vectores canónicos:", determinant)
 #si el determinante es diferente de cero, entonces los vectores son
 #  linealmente independientes.
+
+
+# 1. Crea un array de 10 números aleatorios entre 0 y 100
+
+array = np.random.randint(0,101, size = 10)
+print(array)
+# 2. Imprime el promedio, máximo y mínimo
+print(f'El máximo es {np.max(array)}')
+print(f'El mínimo es {np.min(array)}')
+print(f'El promedio es {np.mean(array)}')
+
+# 3. Normalízalo (valores entre 0 y 1)
+
+normalization = (array-np.min(array)) / (np.max(array) -np.min(array))
+print(f'''
+Normalizando el arreglo: 
+      {normalization}
+''')
+# 4. Imprime cuántos valores están por encima del promedio original
+
+count = 0
+promedio = np.mean(array)
+for dato in array:
+    if dato > promedio:
+        count+=1
+print(f"Hay {count} datos por encima del primedio")
+count = 0
+count = np.sum(array > promedio)
+print(count)

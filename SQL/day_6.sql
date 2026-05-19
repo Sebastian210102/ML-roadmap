148fbb20b871f032c9fd1dd92dea82701b6303e6
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-05-18 23:31:19 -0600

    Day 6: Funciones de ventana

--- SQL/day_6.sql
+++ SQL/day_6.sql
@@ -31,3 +31,47 @@ SELECT t.'Título de la Serie', f.'Fecha del Primer Episodio'  FROM TituloSerie 
 INNER join FechaSerie f 
 ON t.serie_id = f.serie_id
 ORDER BY f.'Fecha del Primer Episodio' ASC
+
+#Ejercicio 2
+
+WITH lanzamientos AS (
+    SELECT año_lanzamiento, COUNT(año_lanzamiento) AS total_series FROM Series
+    WHERE genero = 'Ciencia ficción'
+    GROUP BY año_lanzamiento
+)
+
+SELECT * FROM lanzamientos
+
+
+/*
+Funciones de ventana: 
+
+Realiza cálculosa través de un conjunto de filas de la tabla que están relacionadas 
+con la fila actual
+*/
+
+WITH OrdenSeries AS(
+SELECT titulo, año_lanzamiento
+ROW_NUMBER() OVER (ORDER BY año_lanzamiento DESC) AS ordern_lanzamiento
+FROM Series)
+
+SELECT * FROM OrdenSeries
+WHERE ordern_lanzamiento IN (1,2,3)
+
+# Esto basicamente es asignar un numero el orden es basicamente lo que ponemos en el OVER
+
+/*
+Partition BY
+
+Nos permite dividir el conjunto de resultados de una consulta en particiones
+A las que luego se puede aplicar una funcion
+*/
+
+SELECT
+    titulo,
+    genero,
+    año_lanzamiento
+    ROW_NUMBER() OVER (PARTITION BY genero ORDER BY año_lanzamiento DESC) AS ranking_por_genero
+FROM Series
+
+

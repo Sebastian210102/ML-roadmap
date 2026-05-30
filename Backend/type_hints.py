2d130cdec8bca99a1cf36e5f805cf1de02498b10
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-05-29 19:46:04 -0600

    Creation FILE Backend

---
+++ Backend/type_hints.py
@@ -0,0 +1,21 @@
+#### Type Hints ##
+
+my_String_variable = "My string variable"
+
+print(my_String_variable)
+print(type(my_String_variable))
+
+
+my_String_variable = 5
+print(my_String_variable)
+print(type(my_String_variable))
+
+
+# tipar las variables
+
+my_typed_variable: str = "My typed string"
+
+my_int : int = "Hola como estas"
+
+print(type(my_int))
+

a923e8a343a87db3c9ec3998c8bd6cb33b041b8d
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-07-03 11:08:24 -0600

    Day 1

---
+++ PythonCurso/dia1/print.py
@@ -0,0 +1,16 @@
+#Strings
+
+print("Hola Mundo")
+print("Hola" + " " +"Sebastian")
+
+print("MI nombre es \"Sebastian\"")
+print("\tQue feliz \nEstoy por programar")
+
+#Input
+
+saludo = input("Da un saludo: ")
+print(saludo)
+
+nombre = input("Dame tu nombre: ")
+
+print(f"{saludo} {nombre}")
---
+++ PythonCurso/dia1/proyecto.py
@@ -0,0 +1,6 @@
+#Saber como se llama la cerveza
+
+nombre = input("¿Como te llamas?")
+video_juego = input("Juego favorito")
+
+print(f"El nombre de tu cerveza es '{nombre + video_juego}'")

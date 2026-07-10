52dc5f7dc7b377d247e7f670caea4cfc533c728e
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-07-10 10:58:41 -0600

    Re, add the funtion round

--- PythonCurso/dia2/proyecto.py
+++ PythonCurso/dia2/proyecto.py
@@ -6,4 +6,4 @@ se regresa su nombre y su comisión
 nombre = input("Favor de ingresar tu nombre: ")
 ventas = int(input("Cuales fueron tus ventas de este año?: "))
 
-print(f"Hola, {nombre}!! \nTus comiciones con respecto a tus ventas ({ventas}) es : ${ventas*.13}")
+print(f"Hola, {nombre}!! \nTus comiciones con respecto a tus ventas ({ventas}) es : ${round(ventas*.13,2)}")
